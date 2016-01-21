48ed35ac0b5b260df9e4cd8b2b921194d732b3e0
Gormit <night_wolf_alex@tut.by>
2016-01-21 21:30:34 +0300

    Initial commit

---
+++ desktop/src/by/gormit/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package by.gormit.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import by.gormit.Drop;
+import by.gormit.GameScreen;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Drop";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Drop(), config);
+	}
+}
